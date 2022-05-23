9f2ccd9f07b34c4187c93a8fc48db27400de8a8e
joyopo <104402961+joyopo@users.noreply.github.com>
2022-05-23 20:17:58 -0300

    Delete schedule.py

--- schedule.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from . import file_download
-
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(file_download.main, 'interval', hours=12)
-    scheduler.start()
